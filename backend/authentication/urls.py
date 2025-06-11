12a78c471e5fa4abb94a3a837cc0c6eac161e3d9
arturoCoca8 <kendhycocahuamani8@gmail.com>
2025-06-10 19:29:09 -0500

    Update urls.py
    Mensaje

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import RegisterView, LoginView, UserProfileView
+from django.http import JsonResponse
 
 urlpatterns = [
+    path('', lambda request: JsonResponse({'status': 'API Auth funcionando correctamente'})),  
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
